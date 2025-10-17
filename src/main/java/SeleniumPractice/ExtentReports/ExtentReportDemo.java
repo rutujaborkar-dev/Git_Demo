1d8f7d01fa138ce1cb5a93e604e085140feb676e
Rutuja <rutujaborkar300@gmail.com>
2025-10-17 19:33:24 +0530

    first commit

---
+++ src/main/java/SeleniumPractice/ExtentReports/ExtentReportDemo.java
@@ -0,0 +1,45 @@
+package SeleniumPractice.ExtentReports;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ExtentReportDemo
+{
+        public ExtentReports extent;
+
+        @BeforeTest
+        public void  config()
+        {
+            //Extentreports, ExtentSparkReporter --> responsible for creating html report
+            String path = System.getProperty("user.dir")+"\\reports\\index.html";
+            ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+            reporter.config().setReportName("Web Automation Results");
+            reporter.config().setDocumentTitle("Test Results");
+
+            //responsible to drive report execution
+            extent = new ExtentReports();
+            extent.attachReporter(reporter);
+            extent.setSystemInfo("Tester","Rutuja");
+
+
+        }
+
+        @Test
+        public void initialDemo()
+        {
+            extent.createTest("Initial Demo");
+            System.setProperty("webdriver.chrome.driver",
+                    "C:/Users/rutujab/OneDrive - AMDOCS/Desktop/personal/chromedriver-win64/chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+
+            driver.get("https://rahulshettyacademy.com/");
+            System.out.println(driver.getTitle());
+            extent.flush();
+        }
+
+
+    }
